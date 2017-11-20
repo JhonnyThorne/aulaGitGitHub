939442458f8d3581878da6ea50af7df04335c9d4
JhonnyThorne <jonathansilvaviana@gmail.com>
2017-11-20 12:31:47 -0200

    Update arq.js

--- arq.js
+++ arq.js
@@ -1,12 +1,10 @@
 function soma(){
-	var n1, n2;
+	var n1, n2, soma;
 
 	n1 = document.getElementById ("primeiroNumero").value;
 	n2 = document.getElementById ("segundoNumero").value;
 
-	n1 = parseInt (n1);
-	n2 = parseInt (n2);
-
+	
 	soma = n1 + n2;
 
 	document.getElementById ("resposta").innerHTML = soma;
